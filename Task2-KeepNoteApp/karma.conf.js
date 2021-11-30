71ba15041b56cf086e59ba329504f581b1c3043d
gaurav singh <gauravdarkrider121@gmail.com>
2021-11-30 22:42:54 +0530

    done

--- Task2-KeepNoteApp/karma.conf.js
+++ Task2-KeepNoteApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/keep'),
+      dir: require('path').join(__dirname, './coverage/task-masonry'),
       subdir: '.',
       reporters: [
         { type: 'html' },
